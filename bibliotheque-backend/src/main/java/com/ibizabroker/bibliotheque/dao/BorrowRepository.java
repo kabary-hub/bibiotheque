65261e331a2ea4749bbf81d4f4a894686336f2f0
Ian Ondoua <g.ondoua@pkf-researchcenter.com>
2026-08-14 10:09:32 +0100

    refactor: renomme le projet en bibliotheque
    Remplace le sigle "lms" par "bibliotheque" dans l'ensemble du projet :
    
    - lms-backend  -> bibliotheque-backend
    - lms-frontend -> bibliotheque-frontend
    - package com.ibizabroker.lms -> com.ibizabroker.bibliotheque
    - LmsApplication -> BibliothequeApplication (et sa classe de test)
    - nom du schema MySQL, artifactId Maven, nom du projet Angular
    
    Aucune version de dependance n'est modifiee.

---
+++ bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/dao/BorrowRepository.java
@@ -0,0 +1,13 @@
+package com.ibizabroker.bibliotheque.dao;
+
+import com.ibizabroker.bibliotheque.entity.Borrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BorrowRepository extends JpaRepository<Borrow, Integer> {
+    List<Borrow> findByUserId(Integer userId);
+    List<Borrow> findByBookId(Integer bookId);
+}
---
+++ bibliotheque-backend/src/main/java/com/ibizabroker/bibliotheque/dao/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.ibizabroker.bibliotheque.dao;
+
+import com.ibizabroker.bibliotheque.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByUsername(String username);
+}
